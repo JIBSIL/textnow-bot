830f710075f78a16150db1ccac458114ccdd2b55
JIBSIL <40243545+JIBSIL@users.noreply.github.com>
2020-12-05 10:07:54 -0500

    Update index.js

--- examples/send-message/index.js
+++ examples/send-message/index.js
@@ -1,18 +1,20 @@
 (async () => {
   const puppeteer = require('puppeteer');
   const textnow = require('textnow-bot');
+  const commander = require('commander')
+  commander
+      .version('v0.0.1')
+      .description('Send a TextNow message')
+      .option('--username <type>', 'To input a username')
+      .option('--password <type>', 'To input a password')
+      .option('--sendto <type>', 'To input a recipient')
+      .option('--message <type>', 'To input a message')
+      .parse(process.argv)
   const fs = require('fs').promises;
 
   let browser = null;
   let page = null;
 
-  // Account credentials
-  const username = '';
-  const password = '';
-  // Recipient info
-  const recipient = '';
-  // Message info
-  const message = 'message';
 
   try {
     browser = await puppeteer.launch({ headless: true });
@@ -39,7 +41,7 @@
     catch (error) {
       console.debug('Failed to log in with existing cookies.');
       console.debug('Logging in with account credentials...');
-      cookies = await textnow.logIn(page, client, username, password);
+      cookies = await textnow.logIn(page, client, commander.username, commander.password);
     }
 
     // Save cookies to file
@@ -48,11 +50,11 @@
 
     // Select a conversation using recipient info
     console.debug('Selecting conversation...');
-    await textnow.selectConversation(page, recipient);
+    await textnow.selectConversation(page, commander.sendto);
 
     // Send a message to the current recipient
     console.debug('Sending message...');
-    await textnow.sendMessage(page, message);
+    await textnow.sendMessage(page, commander.message);
 
     console.debug('Message sent!');
     await browser.close();
